2c9e96e933153324def7efea9e62558d659faff6
Astrako <apamarin@gmail.com>
2020-03-10 16:42:58 +0100

    universal7870: universal7870: add Vulkan and OpenCL support

--- universal7870-common-vendor.mk
+++ universal7870-common-vendor.mk
@@ -140,3 +140,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/universal7870-common/proprietary/vendor/lib64/vendor.samsung.hardware.radio.bridge@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio.bridge@2.0.so \
     vendor/samsung/universal7870-common/proprietary/vendor/lib64/vendor.samsung.hardware.radio.channel@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio.channel@2.0.so \
     vendor/samsung/universal7870-common/proprietary/vendor/lib64/vendor.samsung.hardware.radio@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio@2.0.so
+
+# Vulkan and OpenCL
+PRODUCT_PACKAGES += libGLES_mali
