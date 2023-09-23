20120bb9c7fedd6f9eee4d04399246ee8b333e90
Flominator <github.flominatorgd@gmail.com>
2023-09-23 12:38:20 +0200

    universal7870-common: patch audio to stop overrideing libtinyalsa and libaudioroute

--- universal7870-common-vendor.mk
+++ universal7870-common-vendor.mk
@@ -168,7 +168,7 @@ PRODUCT_PACKAGES += libGLES_mali
 # Common audio
 PRODUCT_COPY_FILES += \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/libaudio-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudio-ril.so \
-    vendor/samsung/universal7870-common/proprietary/audio/common/lib/libaudioroute.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudioroute.so \
+    vendor/samsung/universal7870-common/proprietary/audio/common/lib/libaudioroute.universal7870.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudioroute.universal7870.so \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/libLifevibes_lvverx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libLifevibes_lvverx.so \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/libLifevibes_lvvetx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libLifevibes_lvvetx.so \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/libpreprocessing_nxp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpreprocessing_nxp.so \
@@ -179,4 +179,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/libsecaudioinfo.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsecaudioinfo.so \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/lib_soundaliveresampler.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_soundaliveresampler.so \
     vendor/samsung/universal7870-common/proprietary/audio/common/lib/lib_SoundAlive_SRC384_ver320.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_SoundAlive_SRC384_ver320.so \
-    vendor/samsung/universal7870-common/proprietary/audio/common/lib/libtinyalsa.so:$(TARGET_COPY_OUT_VENDOR)/lib/libtinyalsa.so
+    vendor/samsung/universal7870-common/proprietary/audio/common/lib/libtinyalsa.universal7870.so:$(TARGET_COPY_OUT_VENDOR)/lib/libtinyalsa.universal7870.so
